582e6fafe2aaad2178a5045bad893293458405eb
cunhafelipe098 <cunhafelipe098@gmail.com>
2023-03-26 21:07:18 -0300

    feat: create header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export function Header() {
+    return(
+    <header className={styles.header}>
+        <strong>Mini Projeto</strong>
+    </header>
+    );
+}
